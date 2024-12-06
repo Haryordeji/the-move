fa6ae244c8e24bcf7867f3ff36dacb27366f2d2b
Ayo Ogunsanya <aaolamiji@gmail.com>
2024-12-06 10:27:26 -0500

    update seeding file for testusers

--- wtm-server/src/seeders/TestUsers.ts
+++ wtm-server/src/seeders/TestUsers.ts
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import User from '../models/User';
-import { UUID } from 'mongodb';
-
+import { randomUUID } from 'crypto';
 dotenv.config();
 
 const seedDatabase = async () => {
@@ -16,8 +15,8 @@ const seedDatabase = async () => {
         // I am adding fake users here
         await User.deleteMany(); 
         await User.insertMany([
-            { id: new UUID(), username: 'john_doe', email: 'john@example.com', age: 25, college: 'Princeton University', friends: [] },
-            { username: 'jane_doe', email: 'jane@example.com', age: 22, college: 'Harvard University', friends: [] },
+            { user_id: randomUUID(), username: 'john_doe', email: 'john@example.com', age: 25, college: 'Princeton University' },
+            { user_id: randomUUID(),username: 'jane_doe', email: 'jane@example.com', age: 22, college: 'Harvard University' },
         ]);
 
         console.log('Seeding done.');
